768a07233ca9eb1b2ba1e3daf7ca071d94f4d0e8
N RAGHAV <nraghav1996@gmail.com>
2017-03-28 15:19:32 +0530

    jbbnbnbn

---
+++ app/src/main/java/uvote/app/raghav/com/uvote/MainActivity.java
@@ -0,0 +1,15 @@
+package uvote.app.raghav.com.uvote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uvote.app.raghav.com.uvote.uvote.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

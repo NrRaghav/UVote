05ba01006e7374d8a1664d20118a8116d89bd492
chiragsam <chiraggsam@gmail.com>
2017-04-11 11:43:35 +0530

    sdhjh

--- app/src/main/java/uvote/app/raghav/com/uvote/MainActivity.java
+++ app/src/main/java/uvote/app/raghav/com/uvote/MainActivity.java
@@ -4,12 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseIndexRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager mLayoutManager;
 
     DatabaseReference ref;
+    users u = new users();
     private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +41,55 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerview);
 
+        getUser();
+
         ref = FirebaseDatabase.getInstance().getReference().child("surveys");
         mLayoutManager = new LinearLayoutManager(this);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(mLayoutManager);
         mAdapter = new FirebaseRecyclerAdapter<survey,SurveyHolder>(survey.class, R.layout.surveycard, SurveyHolder.class, ref) {
             @Override
-            public void populateViewHolder(SurveyHolder ViewHolder, final survey survey, int position) {
+            public void populateViewHolder(SurveyHolder ViewHolder, final survey survey, int position)
+            {
+                ViewHolder.yes.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                    ArrayList<String> yea;
+                        ArrayList<String> no;
+                        yea=survey.getYes();
+                        no=survey.getNo();
+                        if (!yea.contains(u.getRno())&&!no.contains(u.getRno())){
+                            yea.add(u.getRno());
+                            ref.child(survey.getSurveyid()).child("yes").setValue(yea);
+                        }
+                        else
+                        {
+                            Toast.makeText(MainActivity.this, "You can only vote once!!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+
+
+                    }
+                });
+                ViewHolder.no.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ArrayList<String> yea;
+                        ArrayList<String> no;
+                        yea=survey.getYes();
+                        no=survey.getNo();
+                        if (!yea.contains(u.getRno())&&!no.contains(u.getRno())){
+                            no.add(u.getRno());
+                            ref.child(survey.getSurveyid()).child("no").setValue(no);
+                        }
+                        else
+                        {
+                            Toast.makeText(MainActivity.this, "You can only vote once!!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
                 ViewHolder.tv.setText(survey.getDescription());
                 Glide.with(MainActivity.this).load(survey.getPicture()).into(ViewHolder.iv);
             }
@@ -55,5 +105,24 @@ public class MainActivity extends AppCompatActivity {
         ref = mDatabase.child("surveys");
 
 
+    }
+    void getUser()
+    {
+        FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+        final String uid=mUser.getUid();
+        final DatabaseReference mDatabase= FirebaseDatabase.getInstance().getReference().child("users").child(uid);
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                u=dataSnapshot.getValue(users.class);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
